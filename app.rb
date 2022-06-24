cacdc5057dedd9b5e1e9fd3ce9ff14a0b6e53194
EC2 Default User <ec2-user@ip-10-1-9-78.ap-northeast-1.compute.internal>
2022-06-24 02:37:16 +0000

    チェックワーク形式に

--- app.rb
+++ app.rb
@@ -3,12 +3,6 @@ Bundler.require
 require 'sinatra/reloader' if development?
 require './models/history.rb'
 
-#before do
-#  if Count.all.size == 0
-#    Count.create(number: 0)
-#  end
-#end
-
 before do
   if History.all.size == 0
     History.create(small: 0, good: 0, excellent: 0, total: 0)
@@ -20,29 +14,30 @@ get '/' do
 end
 
 get '/result' do
-  @uname = params[:username]
+  #index.erbのinputタグに入力された名前はどうやって取り出す？
+  #取り出した値を次のresult.erbでも使うためにはどうすれば？
+  
+  #以下抽選機能
   #rnum = 0から9までの乱数
   rnum = rand(10)
   history = History.find(1)
   
-  #rnumの値に応じてインスタンス変数@hisultの値を変える
+  #rnumの値に応じてインスタンス変数@resultの値を変える
   #確率はrnumの大きさを図る部分で変更
   if rnum < 6
     @result = "小吉!"
     history.small = history.small + 1
-    @color = "#009688"
   elsif rnum < 9
     @result = "中吉!"
     history.good = history.good + 1
-    @color = "#d36015"
   else 
     @result = "大吉!"
     history.excellent = history.excellent + 1
-    @color = "#dc143c"
   end
   
   history.total = history.total + 1
   history.save
-  @history = history
+  #historyテーブルでこれまでの占い結果を管理している
+  #このテーブルに入ったレコードをresult.erbで用いれば表が作れそう？
   erb :result
 end
